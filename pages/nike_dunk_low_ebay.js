69742eabd8849cd20ad335cc3f44c413bf89a2fe
3thn <65327271+yk3thn@users.noreply.github.com>
2024-05-06 02:53:13 -0400

    Add files via upload

---
+++ pages/nike_dunk_low_ebay.js
@@ -0,0 +1,37 @@
+const images = ["../assets/items/nike_dunk_low_ebay/1.png", "../assets/items/nike_dunk_low_ebay/2.png", "../assets/items/nike_dunk_low_ebay/3.png", "../assets/items/nike_dunk_low_ebay/4.png", "../assets/items/nike_dunk_low_ebay/5.png"];
+let currentIndex = 0;
+//document.getElementById("mainImage").src = images[currentIndex];
+
+function prevImage() {
+  if (currentIndex === 0) {
+    currentIndex = images.length - 1;
+  } else {
+    currentIndex--;
+  }
+  document.getElementById("mainImage").src = images[currentIndex];
+}
+
+function nextImage() {
+  if (currentIndex === images.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  document.getElementById("mainImage").src = images[currentIndex];
+}
+const btn = document.querySelector('.btn');
+    const imageGridOverlay = document.querySelector('.image-grid-overlay');
+    const closeBtn = document.createElement('span');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.className = 'image-grid-close';
+    imageGridOverlay.appendChild(closeBtn);
+
+    btn.addEventListener('click', () => {
+        imageGridOverlay.classList.add('active');
+        document.body.style.overflow = 'hidden'; // Prevent scrolling
+    });
+
+    closeBtn.addEventListener('click', () => {
+        imageGridOverlay.classList.remove('active');
+        document.body.style.overflow = ''; // Re-enable scrolling
+    });
