5b357c9ad801e0e693a2b30eee83404b55066af3
yk3thn <yk3thn@gmail.com>
2025-10-12 19:52:44 -0400

    Quick update

--- contact.js
+++ contact.js
@@ -1,22 +1,10 @@
+
+		
 function adjustStylesForScreenSize() {
-    
+    var screenWidth = window.innerWidth;
 
-var screenWidth = window.innerWidth;
-
-    if (screenWidth <= 1300){
-        document.getElementById('navbtn').style.fontSize = '10px';
-		document.getElementById('navbtn2').style.fontSize = '10px';
-		document.getElementById('LLogo').style.top = '12.5%';
-		document.getElementById('RLogo').style.top = '12.5%';
-		document.getElementById('MLogo').style.top = '5%';
-        document.getElementById('searchInput').style.fontSize = '12px';
-        document.getElementById('searchInput').style.width = '100px';
-        document.getElementById('searchContainer').style.padding = '5px';
-        document.getElementById('navbtn').style.padding = '5px 10px';
-		document.getElementById('navbtn2').style.padding = '5px 10px';
-        document.querySelector('.grid-container').style.marginTop = '40px';
-		document.getElementById('banner').style.fontSize = '10px';
-document.getElementById('logoWordmark').style.width = '125px';
+    if (screenWidth <= 1300) { // Adjust styles for smaller screens
+        document.getElementById('logoWordmark').style.width = '125px';
 		document.getElementById('logoHover').style.width = '125px';
 		document.getElementById('banner').style.fontSize = '10px';
 		document.getElementById('responsiveText').style.fontSize = 	'25px';
@@ -34,21 +22,9 @@ document.getElementById('logoWordmark').style.width = '125px';
 		document.getElementById('responsiveTextc').style.fontSize = '15px';
 		document.getElementById('responsiveTextd').style.fontSize = '20px';
 		document.getElementById('responsiveTexte').style.fontSize = '15px';
-    } else {
-        document.getElementById('navbtn').style.fontSize = '20px';
-		document.getElementById('navbtn2').style.fontSize = '20px';
-		document.getElementById('LLogo').style.top = '27%';
-		document.getElementById('RLogo').style.top = '27%';
-		document.getElementById('MLogo').style.top = '1%';
-        document.getElementById('searchInput').style.fontSize = '16px';
-        document.getElementById('searchInput').style.width = '200px';
-        document.getElementById('searchContainer').style.padding = '10px';
-        document.getElementById('navbtn').style.padding = '7px 15px';
-		document.getElementById('navbtn2').style.padding = '7px 15px';
-        document.querySelector('.grid-container').style.marginTop = '50px';
-		document.getElementById('banner').style.fontSize = '15px';
-document.getElementById('logoWordmark').style.width = '200px';
-document.getElementById('logoHover').style.width = '200px';
+    } else { // Reset styles for larger screens
+        document.getElementById('logoWordmark').style.width = '200px';
+		document.getElementById('logoHover').style.width = '200px';
 		document.getElementById('banner').style.fontSize = '15px';
 		document.getElementById('responsiveText').style.fontSize = 	'40px';
 		document.getElementById('responsiveText1').style.fontSize = '30px';
@@ -67,8 +43,6 @@ document.getElementById('logoHover').style.width = '200px';
 		document.getElementById('responsiveTexte').style.fontSize = '20px';
     }
 }
-
-
 
 // Call the function initially and on window resize
 adjustStylesForScreenSize();
